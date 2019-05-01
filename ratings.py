e98897f67de516e23f2ff21ff3f3df21cd1a84a4
Fellowship Student <hackbright@hackbrightacademy.com>
2019-04-30 21:01:35 -0700

    add new function for new rating

--- ratings.py
+++ ratings.py
@@ -10,14 +10,20 @@ def get_restaurant_info(file):
 		restaurant_ratings[restaurant[0]] = restaurant[1]
 	restaurant_data.close()
 
-	new_restaurant_name = input("Can we get the name of the restaurant?")
-	restaurant_score = input("Give it a rating")
-	restaurant_ratings[new_restaurant_name] = restaurant_score
+	new_rating = request_new_rating()
+	restaurant_ratings = {**restaurant_ratings, **new_rating}
 
 	restaurants_sorted = sorted(list(restaurant_ratings.keys()))
 	for restaurant_sorted in restaurants_sorted:
 		print('{} is rated at {}'.format(restaurant_sorted, restaurant_ratings[restaurant_sorted]))
 
-get_restaurant_info("scores.txt")
+def request_new_rating():
+	new_rating = {}
+	new_restaurant_name = input("Can we get the name of the restaurant?")
+	restaurant_score = input("Give it a rating")
+	new_rating[new_restaurant_name] = restaurant_score
+	return new_rating
 
+get_restaurant_info("scores.txt")
+#request_new_rating()
 
